128f491e6a8a666cc2cb704bc13d2abc9783b7a6
Dwijraj Bhattacharyya <dwijrajbhattacharyya@gmail.com>
2025-05-01 17:59:21 +0000

    Validator base created

--- linear-regression/LinearRegression.py
+++ linear-regression/LinearRegression.py
@@ -11,20 +11,6 @@ class LinearRegression:
         pass
 
     def __check(self, feature_data, label):
-        if feature_data == None or label == None:
-            raise ValueError("Invalid input!")
-        
-        if isinstance(feature_data, np.ndarray):
-            raise ValueError("Not valid training data of numpy array input!")
-
-        if isinstance(label, np.ndarray):
-            raise ValueError("Not valid label of array input!")
-        
-        shape_features = feature_data.shape
-        label_shape = label.shape
-
-        if shape_features[1]== label_shape[0]:
-            raise ValueError("Training data shape and label shape don't match")
 
 
 lin = LinearRegression(np.array([1,2]), np.array([2,3]))
---
+++ validators/BaseAlgorithmValidator.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class BaseAlgorithmValidator(ABC):
+
+    def validateInputShape(self, feature_data, label):
+        if feature_data is None or label is None:
+            raise ValueError("Invalid input!")
+        
+        if not isinstance(feature_data, np.ndarray):
+            raise ValueError("Not valid training data of numpy array input!")
+
+        if not isinstance(label, np.ndarray):
+            raise ValueError("Not valid label of array input!")
+
+        shape_features = feature_data.shape
+        label_shape = label.shape
+
+        if shape_features[1]== label_shape[0]:
+            raise ValueError("Training data shape and label shape don't match")
