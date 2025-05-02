68e7606616fe68e0b6880b4e05132b419d1026e2
Dwijraj Bhattacharyya <dwijrajbhattacharyya@gmail.com>
2025-05-02 07:40:48 +0000

    Data cleaning integrated

--- jupyter-notebooks/jupyterutils.py
+++ jupyter-notebooks/jupyterutils.py
@@ -5,6 +5,7 @@ import seaborn as sns
 import sympy as sp
 import os
 import sys
+from sklearn.preprocessing import StandardScaler
 
 # Add project root to sys.path for clean imports
 sys.path.append(os.path.abspath(".."))
@@ -14,3 +15,25 @@ def ensure_column_vector(arr):
     if arr.ndim == 1:
         arr = arr.reshape(-1, 1)
     return arr
+
+def train_test_split(X, y, training_percent = 0.85):
+    df = pd.DataFrame(X)
+    df['label'] = y
+    df = df.dropna()  # or use df.fillna() with strategy
+    df = df.drop_duplicates()
+    scaler = StandardScaler()
+    df.iloc[:, :-1] = scaler.fit_transform(df.iloc[:, :-1])  # all but label
+    df = df[df['label'].notna()]
+    X_cleaned = df.drop('label', axis=1).values
+    y_cleaned = df['label'].values
+    samples = X_cleaned.shape[0]
+
+    training_numbers = int(training_percent*samples)
+
+    training_features = ensure_column_vector(np.array(X_cleaned[:training_numbers]))
+    training_labels = np.array(y_cleaned[:training_numbers])
+
+    testing_attributes=ensure_column_vector(np.array(X_cleaned[training_numbers:]))
+    testing_labels=np.array(y_cleaned[training_numbers:])
+
+    return training_features, training_labels, testing_attributes, testing_labels
