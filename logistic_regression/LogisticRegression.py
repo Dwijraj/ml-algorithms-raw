f6cd9a244e0385ce6941349247dcadbac33839d8
Dwijraj Bhattacharyya <dwijrajbhattacharyya@gmail.com>
2025-05-02 10:07:27 +0000

    Boiler plat code added for logistic regression

--- logistic_regression/LogisticRegression.py
+++ logistic_regression/LogisticRegression.py
@@ -22,26 +22,7 @@ class LogisticRegression:
         self._loss=[]
 
     def train(self, iterations, logTrainingParams= False):
-
-        self._loss=[]
-        for i in range(iterations):
-            predictions = np.dot(self.feature_data, self.weight) + self.bias
-
-            # Parital differentiation of cost function MSE J = sum(( prediction - actual)^2)/sample_count
-            # def calculateMseLoss(actual, predicted)
-            # dJ/dW = 2*X*(prediction - actual)/sample_count
-            # dJ/dB = 2*(prediction - actual)/sample_count
-            error = predictions-self.label
-            dW = ((2*np.dot(self.feature_data.T , error))/self.sample_count)
-            dJ = ((2*np.sum(error))/self.sample_count)
-
-            self._weight = self._weight- self.learning_rate*dW
-            self._bias = self._bias - self.learning_rate*dJ
-            loss = self.calculateMseLoss(self.label, predictions)
-            self._loss.append(loss)
-
-            if logTrainingParams and i%1000 == 0:
-                print("Iteration ", i , " weights ", self.weight, " bias ", self.bias, "loss" , loss)
+        pass
 
     def predict(self, attributes):
         return 1 if self.__sigmoidFunction(np.dot(attributes,self.weight)+self.bias) >= self.threshold else 0
@@ -52,12 +33,8 @@ class LogisticRegression:
             z = np.clip(z, -500, 500)                   
             return 1.0 / (1.0 + np.exp(-z))
     
-    def calculateMseLoss(self,actual, predicted):
-        actual    = np.asarray(actual).reshape(-1)
-        predicted = np.asarray(predicted).reshape(-1)
-        if actual.shape != predicted.shape:
-            raise ValueError("Length mismatch")
-        return np.mean((actual - predicted) ** 2)
+    def calculateBinaryCrossEntropyLoss(self,actual, predicted):
+        pass
         
 
     @property
